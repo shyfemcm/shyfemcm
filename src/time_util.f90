fbc76bdf3a55ab12c0ccb4ecd270f92f8e89d56d
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2023-10-29 17:21:54 +0100

    time_util.f90 log 7

--- src/time_util.f90
+++ src/time_util.f90
@@ -24,110 +24,110 @@
 !
 !--------------------------------------------------------------------------
 
-c time management routines
-c
-c contents :
-c
-c
-c revision log :
-c
-c 23.09.1997	ggu	boundn deleted -> no access to data structure
-c 20.03.1998	ggu	minor changes to priout
-c 29.04.1998	ggu	new module for semi-implicit time-step
-c 07.05.1998	ggu	check for error on return of nrdvecr
-c 19.06.1998	ggu	version number is character
-c 22.01.1999	ggu	oxygen section added
-c 26.01.1999	ggu	new comp3d added
-c 11.08.1999	ggu	new compatibility array hlhv initialized
-c 19.11.1999	ggu	new routines for section vol
-c 20.01.2000	ggu	common block /dimdim/ eliminated
-c 04.02.2000	ggu	no priout, dobefor/after, pritime, endtime
-c 15.05.2000	ggu	hm3v substituted
-c 26.05.2000	ggu	copright statement adjourned
-c 21.11.2001	ggu	routines to handle advective index (aix)
-c 27.11.2001	ggu	routine to handle info file (getinfo)
-c 11.10.2002	ggu	aix routines deleted
-c 07.02.2003	ggu	routine added: changeimp, getaz; deleted getaza
-c 10.08.2003	ggu	call adjust_chezy instead sp135r
-c 14.08.2003	ggu	femver transfered to subver, not called in nlsh2d
-c 20.08.2003	ggu	tsmed substituted by ts_shell
-c 01.09.2003	ggu	call wrousa
-c 03.09.2004	ggu	call admrst, comp3d renamed to init_3d (not used)
-c 03.09.2004	ggu	nlv, hlv initialized in nlsh2d (FIXME)
-c 28.09.2004	ggu	read lagrangian section
-c 01.12.2004	ggu	new routine set_timestep for variable time step
-c 17.01.2005	ggu	get_stab_index to newcon.f, error stop in set_timestep
-c 14.03.2005	ggu	syncronize idt with end of simulation (set_timestep)
-c 07.11.2005	ggu	handle new section sedtr for sediments
-c 23.03.2006	ggu	changed time step to real
-c 23.05.2007	ggu	recall variable time step pars at every time step
-c 02.10.2007	ggu	bug fix in set_timestep for very small rindex
-c 10.04.2008	ccf	output in netcdf format
-c 28.04.2008	ggu	in set_timestep new call to advect_stability()
-c 03.09.2008	ggu	in nlsh2d different error message
-c 20.11.2008	ggu	init_3d deleted, nlv initialized to 0
-c 18.11.2009	ggu	new format in pritime (write also time step)
-c 22.02.2010	ggu	new call to hydro_stability to compute time step
-c 22.02.2010	ccf	new routine for tidal pot. (tideforc), locaus deleted
-c 26.02.2010	ggu	in set_timestep compute and write ri with old dt
-c 22.03.2010	ggu	some comments for better readability
-c 29.04.2010	ggu	new routine set_output_frequency() ... not finished
-c 04.05.2010	ggu	shell to compute energy
-c 22.02.2011	ggu	in pritime() new write to terminal
-c 20.05.2011	ggu	changes in set_timestep(), element removal, idtmin
-c 31.05.2011	ggu	changes for BFM
-c 01.06.2011	ggu	idtmin introduced
-c 12.07.2011	ggu	new routine next_output(), revised set_output_frequency
-c 14.07.2011	ggu	new routines for original time step
-c 13.09.2011	ggu	better error check, rdtitl() more robust
-c 23.01.2012	ggu	new section "proj"
-c 24.01.2012	ggu	new routine setup_parallel()
-c 10.02.2012	ggu	new routines to initialize and access time common block
-c 05.03.2014	ggu	code prepared to repeat time step (irepeat) - not ready
-c 05.03.2014	ggu	new routines get_last/first_time()
-c 10.04.2014	ccf	new section "wrt" for water renewal time
-c 29.10.2014	ggu	do_() routines transfered from newpri.f
-c 10.11.2014	ggu	time management routines transfered to this file
-c 26.11.2014	ggu	changed VERS_7_0_7
-c 05.12.2014	ggu	changed VERS_7_0_8
-c 12.12.2014	ggu	changed VERS_7_0_9
-c 19.12.2014	ggu	accept date also as string
-c 23.12.2014	ggu	fractional time step introduced
-c 07.01.2015	ggu	fractional time step without rounding (itsplt=3)
-c 26.02.2015	ggu	changed VERS_7_1_5
-c 30.04.2015	ggu	changed VERS_7_1_9
-c 23.09.2015	ggu	time step is now working with dt as double
-c 10.10.2015	ggu	use bsync as global to check for syncronization
-c 23.09.2016	ggu	cleaned set_timestep()
-c 30.09.2016	ggu	changed VERS_7_5_18
-c 20.10.2017	ggu	new get_absolute_act_time(),get_absolute_ref_time()
-c 04.11.2017	ggu	changed VERS_7_5_34
-c 05.12.2017	ggu	changed VERS_7_5_39
-c 22.02.2018	ggu	changed VERS_7_5_42
-c 23.02.2018	ggu	most parts converted from int to double
-c 29.03.2018	ggu	bug fix for syncronization step
-c 03.04.2018	ggu	changed VERS_7_5_43
-c 03.04.2018	ggu	changed VERS_7_5_44
-c 13.04.2018	ggu	hydro_stability includes explicit gravity wave
-c 13.04.2018	ggu	set_timestep now is working with mpi
-c 16.04.2018	ggu	write warning if time step is over recommended one
-c 09.02.2019	ggu	bug fix for syncronization of last time step
-c 16.02.2019	ggu	changed VERS_7_5_60
-c 21.05.2019	ggu	changed VERS_7_5_62
-c 15.09.2019	ggu	small changes to account for synchorization time step
-c 08.02.2020	ggu	utilities in this new file
-c 16.02.2020	ggu	itunit eliminated
-c 16.02.2020	ggu	new routines get_time_iterations(), get_ddt()
-c 03.04.2020	ggu	new routine get_real_time()
-c 18.05.2022	ggu	new routines cpu_time_*()
-c
-c**********************************************************************
-c**********************************************************************
-c**********************************************************************
+! time management routines
+!
+! contents :
+!
+!
+! revision log :
+!
+! 23.09.1997	ggu	boundn deleted -> no access to data structure
+! 20.03.1998	ggu	minor changes to priout
+! 29.04.1998	ggu	new module for semi-implicit time-step
+! 07.05.1998	ggu	check for error on return of nrdvecr
+! 19.06.1998	ggu	version number is character
+! 22.01.1999	ggu	oxygen section added
+! 26.01.1999	ggu	new comp3d added
+! 11.08.1999	ggu	new compatibility array hlhv initialized
+! 19.11.1999	ggu	new routines for section vol
+! 20.01.2000	ggu	common block /dimdim/ eliminated
+! 04.02.2000	ggu	no priout, dobefor/after, pritime, endtime
+! 15.05.2000	ggu	hm3v substituted
+! 26.05.2000	ggu	copright statement adjourned
+! 21.11.2001	ggu	routines to handle advective index (aix)
+! 27.11.2001	ggu	routine to handle info file (getinfo)
+! 11.10.2002	ggu	aix routines deleted
+! 07.02.2003	ggu	routine added: changeimp, getaz; deleted getaza
+! 10.08.2003	ggu	call adjust_chezy instead sp135r
+! 14.08.2003	ggu	femver transfered to subver, not called in nlsh2d
+! 20.08.2003	ggu	tsmed substituted by ts_shell
+! 01.09.2003	ggu	call wrousa
+! 03.09.2004	ggu	call admrst, comp3d renamed to init_3d (not used)
+! 03.09.2004	ggu	nlv, hlv initialized in nlsh2d (FIXME)
+! 28.09.2004	ggu	read lagrangian section
+! 01.12.2004	ggu	new routine set_timestep for variable time step
+! 17.01.2005	ggu	get_stab_index to newcon.f, error stop in set_timestep
+! 14.03.2005	ggu	syncronize idt with end of simulation (set_timestep)
+! 07.11.2005	ggu	handle new section sedtr for sediments
+! 23.03.2006	ggu	changed time step to real
+! 23.05.2007	ggu	recall variable time step pars at every time step
+! 02.10.2007	ggu	bug fix in set_timestep for very small rindex
+! 10.04.2008	ccf	output in netcdf format
+! 28.04.2008	ggu	in set_timestep new call to advect_stability()
+! 03.09.2008	ggu	in nlsh2d different error message
+! 20.11.2008	ggu	init_3d deleted, nlv initialized to 0
+! 18.11.2009	ggu	new format in pritime (write also time step)
+! 22.02.2010	ggu	new call to hydro_stability to compute time step
+! 22.02.2010	ccf	new routine for tidal pot. (tideforc), locaus deleted
+! 26.02.2010	ggu	in set_timestep compute and write ri with old dt
+! 22.03.2010	ggu	some comments for better readability
+! 29.04.2010	ggu	new routine set_output_frequency() ... not finished
+! 04.05.2010	ggu	shell to compute energy
+! 22.02.2011	ggu	in pritime() new write to terminal
+! 20.05.2011	ggu	changes in set_timestep(), element removal, idtmin
+! 31.05.2011	ggu	changes for BFM
+! 01.06.2011	ggu	idtmin introduced
+! 12.07.2011	ggu	new routine next_output(), revised set_output_frequency
+! 14.07.2011	ggu	new routines for original time step
+! 13.09.2011	ggu	better error check, rdtitl() more robust
+! 23.01.2012	ggu	new section "proj"
+! 24.01.2012	ggu	new routine setup_parallel()
+! 10.02.2012	ggu	new routines to initialize and access time common block
+! 05.03.2014	ggu	code prepared to repeat time step (irepeat) - not ready
+! 05.03.2014	ggu	new routines get_last/first_time()
+! 10.04.2014	ccf	new section "wrt" for water renewal time
+! 29.10.2014	ggu	do_() routines transfered from newpri.f
+! 10.11.2014	ggu	time management routines transfered to this file
+! 26.11.2014	ggu	changed VERS_7_0_7
+! 05.12.2014	ggu	changed VERS_7_0_8
+! 12.12.2014	ggu	changed VERS_7_0_9
+! 19.12.2014	ggu	accept date also as string
+! 23.12.2014	ggu	fractional time step introduced
+! 07.01.2015	ggu	fractional time step without rounding (itsplt=3)
+! 26.02.2015	ggu	changed VERS_7_1_5
+! 30.04.2015	ggu	changed VERS_7_1_9
+! 23.09.2015	ggu	time step is now working with dt as double
+! 10.10.2015	ggu	use bsync as global to check for syncronization
+! 23.09.2016	ggu	cleaned set_timestep()
+! 30.09.2016	ggu	changed VERS_7_5_18
+! 20.10.2017	ggu	new get_absolute_act_time(),get_absolute_ref_time()
+! 04.11.2017	ggu	changed VERS_7_5_34
+! 05.12.2017	ggu	changed VERS_7_5_39
+! 22.02.2018	ggu	changed VERS_7_5_42
+! 23.02.2018	ggu	most parts converted from int to double
+! 29.03.2018	ggu	bug fix for syncronization step
+! 03.04.2018	ggu	changed VERS_7_5_43
+! 03.04.2018	ggu	changed VERS_7_5_44
+! 13.04.2018	ggu	hydro_stability includes explicit gravity wave
+! 13.04.2018	ggu	set_timestep now is working with mpi
+! 16.04.2018	ggu	write warning if time step is over recommended one
+! 09.02.2019	ggu	bug fix for syncronization of last time step
+! 16.02.2019	ggu	changed VERS_7_5_60
+! 21.05.2019	ggu	changed VERS_7_5_62
+! 15.09.2019	ggu	small changes to account for synchorization time step
+! 08.02.2020	ggu	utilities in this new file
+! 16.02.2020	ggu	itunit eliminated
+! 16.02.2020	ggu	new routines get_time_iterations(), get_ddt()
+! 03.04.2020	ggu	new routine get_real_time()
+! 18.05.2022	ggu	new routines cpu_time_*()
+!
+!**********************************************************************
+!**********************************************************************
+!**********************************************************************
 
 	subroutine is_time_first(bfirst)
 
-c true if in initialization phase
+! true if in initialization phase
 
 	implicit none
 
@@ -139,11 +139,11 @@ c true if in initialization phase
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
 	subroutine is_time_last(blast)
 
-c true if in last time step
+! true if in last time step
 
 	implicit none
 
@@ -155,11 +155,11 @@ c true if in last time step
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_act_dtime(dtact)
 
-c returns actual time (double)
+! returns actual time (double)
 
         implicit none
 
@@ -171,11 +171,11 @@ c returns actual time (double)
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_timeline(dtime,aline)
 
-c returns time as string
+! returns time as string
 
         implicit none
 
@@ -190,11 +190,11 @@ c returns time as string
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_act_timeline(aline)
 
-c returns actual time as string
+! returns actual time as string
 
         implicit none
 
@@ -206,11 +206,11 @@ c returns actual time as string
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_absolute_act_time(atime)
 
-c returns actual time
+! returns actual time
 
         implicit none
 
@@ -222,11 +222,11 @@ c returns actual time
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_absolute_ref_time(atime_ref)
 
-c returns actual time
+! returns actual time
 
         implicit none
 
@@ -238,11 +238,11 @@ c returns actual time
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_passed_dtime(dtime)
 
-c returns time passed since start of simulation
+! returns time passed since start of simulation
 
         implicit none
 
@@ -254,11 +254,11 @@ c returns time passed since start of simulation
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_first_dtime(dtime)
 
-c returns first (initial) time
+! returns first (initial) time
 
         implicit none
 
@@ -270,11 +270,11 @@ c returns first (initial) time
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_last_dtime(dtime)
 
-c returns end time
+! returns end time
 
         implicit none
 
@@ -286,11 +286,11 @@ c returns end time
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_ddt(ddt)
 
-c returns time step (in seconds - double version)
+! returns time step (in seconds - double version)
 
         implicit none
 
@@ -302,11 +302,11 @@ c returns time step (in seconds - double version)
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_timestep(dt)
 
-c returns time step (in seconds - real version)
+! returns time step (in seconds - real version)
 
         implicit none
 
@@ -318,11 +318,11 @@ c returns time step (in seconds - real version)
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_orig_timestep(dt)
 
-c returns original real time step (in real seconds)
+! returns original real time step (in real seconds)
 
         implicit none
 
@@ -334,11 +334,11 @@ c returns original real time step (in real seconds)
 
 	end
 
-c**********************************************************************
+!**********************************************************************
 
         subroutine get_time_iterations(nit_done,nit_todo)
 
-c returns iterations, already done and still to do
+! returns iterations, already done and still to do
 
         implicit none
 
@@ -352,9 +352,9 @@ c returns iterations, already done and still to do
 
 	end
 
-c********************************************************************
-c********************************************************************
-c********************************************************************
+!********************************************************************
+!********************************************************************
+!********************************************************************
 
 	subroutine convert_to_dtime(aline,dtime)
 
@@ -372,7 +372,7 @@ c********************************************************************
 
 	end subroutine convert_to_dtime
 
-c********************************************************************
+!********************************************************************
 
 	subroutine convert_to_atime(aline,atime)
 
@@ -391,9 +391,9 @@ c********************************************************************
 
 	end subroutine convert_to_atime
 
-c********************************************************************
-c********************************************************************
-c********************************************************************
+!********************************************************************
+!********************************************************************
+!********************************************************************
 
         subroutine get_real_time(atime,aline)
 
@@ -417,9 +417,9 @@ c********************************************************************
 
         end
 
-c********************************************************************
-c********************************************************************
-c********************************************************************
+!********************************************************************
+!********************************************************************
+!********************************************************************
 
         subroutine cpu_time_init
 
@@ -432,7 +432,7 @@ c********************************************************************
 
         end
 
-c********************************************************************
+!********************************************************************
 
         subroutine cpu_time_start(itime)
 
@@ -452,7 +452,7 @@ c********************************************************************
 
 	end
 
-c********************************************************************
+!********************************************************************
 
         subroutine cpu_time_end(itime)
 
@@ -474,7 +474,7 @@ c********************************************************************
 
         end
 
-c********************************************************************
+!********************************************************************
 
         subroutine cpu_time_get(itime,time)
 
@@ -492,7 +492,7 @@ c********************************************************************
 
         end
 
-c********************************************************************
-c********************************************************************
-c********************************************************************
+!********************************************************************
+!********************************************************************
+!********************************************************************
 
