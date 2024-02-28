b9fb4055588116386870e90251c91a9e4b64f40b
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2024-02-28 11:41:07 +0100

    new routines in bin

--- src/utils/shyutil/version.f90
+++ src/utils/shyutil/version.f90
@@ -396,7 +396,7 @@
 !
 ! \newcommand{\VERSION}{8.0.2}
 ! \newcommand{\version}{8\_0\_2}
-! \newcommand{\COMMIT}{2023-10-23}
+! \newcommand{\COMMIT}{2024-02-28}
 !
 ! DOCS	END
 
@@ -405,7 +405,7 @@
 	logical, save		:: bshort = .false.
 
         character*10, parameter :: version = '8.0.2'
-        character*10, parameter :: commit  = '2023-10-23'
+        character*10, parameter :: commit  = '2024-02-28'
         character*17, parameter :: text    = 'SHYFEM VERSION = '
 
         character*40, parameter :: string = text//version//'  '//commit
