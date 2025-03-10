1fdb8661a9645f6dd5c4a946d881e331bc9a176f
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2025-03-10 18:27:38 +0100

    perl script for local commit

--- src/utils/shyutil/version.f90
+++ src/utils/shyutil/version.f90
@@ -420,7 +420,7 @@
 
         character*10, parameter :: version = '8.2.4'
         character*10, parameter :: commit  = '2025-03-09'
-        character*50, parameter :: lcommit  = ''
+        character*50, parameter :: lcommit = '8.2.4-mpi-1'
         character*17, parameter :: text    = 'SHYFEM VERSION = '
 
         character*40, parameter :: string = text//version//'  '//commit
